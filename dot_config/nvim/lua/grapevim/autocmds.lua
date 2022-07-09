a963d511eda5c83e9305728d23cbd03c874c6d05
Danmar Varela <vareladanmar@gmail.com>
2022-07-09 15:01:53 +0800

    Change trigger events for openfolds and formatting autocommands

--- dot_config/nvim/lua/grapevim/autocmds.lua
+++ dot_config/nvim/lua/grapevim/autocmds.lua
@@ -56,8 +56,8 @@ function M.load()
     end
   })
 
-  -- Open all folds when opening a file
-  vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  -- Open all folds when opening and reading a buffer
+  vim.api.nvim_create_autocmd({ "BufWinEnter", "BufRead" }, {
     pattern = { "*" },
     command = "let &foldlevel = max(map(range(1, line('$')), 'foldlevel(v:val)'))",
     group = vim.api.nvim_create_augroup("OpenFolds", {})
@@ -71,7 +71,7 @@ function M.load()
   })
 
   -- Format buffer before writing it to a file
-  vim.api.nvim_create_autocmd({ "BufWrite" }, {
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = { "*" },
     callback = function()
       vim.lsp.buf.formatting_sync({})
