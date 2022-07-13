2301a4dbb25f132c9b7bd434e4f6397901bb8a1d
Danmar Varela <danmarvarela@Mitzs-MacBook-Air.local>
2022-07-13 10:04:22 +0800

    Format file

--- dot_config/nvim/lua/grapevim/core/cmp.lua
+++ dot_config/nvim/lua/grapevim/core/cmp.lua
@@ -1,145 +1,145 @@
 local M = {}
 
 M.cmp_kinds = {
-  Text = '  ',
-  Method = '  ',
-  Function = '  ',
-  Constructor = '  ',
-  Field = '  ',
-  Variable = '  ',
-  Class = '  ',
-  Interface = '  ',
-  Module = '  ',
-  Property = '  ',
-  Unit = '  ',
-  Value = '  ',
-  Enum = '  ',
-  Keyword = '  ',
-  Snippet = '  ',
-  Color = '  ',
-  File = '  ',
-  Reference = '  ',
-  Folder = '  ',
-  EnumMember = '  ',
-  Constant = '  ',
-  Struct = '  ',
-  Event = '  ',
-  Operator = '  ',
-  TypeParameter = '  ',
+	Text = "  ",
+	Method = "  ",
+	Function = "  ",
+	Constructor = "  ",
+	Field = "  ",
+	Variable = "  ",
+	Class = "  ",
+	Interface = "  ",
+	Module = "  ",
+	Property = "  ",
+	Unit = "  ",
+	Value = "  ",
+	Enum = "  ",
+	Keyword = "  ",
+	Snippet = "  ",
+	Color = "  ",
+	File = "  ",
+	Reference = "  ",
+	Folder = "  ",
+	EnumMember = "  ",
+	Constant = "  ",
+	Struct = "  ",
+	Event = "  ",
+	Operator = "  ",
+	TypeParameter = "  ",
 }
 
 function M.setup()
-  local cmp_exists, cmp = pcall(require, "cmp")
-  if not cmp_exists then
-    return
-  end
+	local cmp_exists, cmp = pcall(require, "cmp")
+	if not cmp_exists then
+		return
+	end
 
-  local luasnip = require("luasnip")
+	local luasnip = require("luasnip")
 
-  local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  end
+	local has_words_before = function()
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	end
 
-  cmp.setup({
-    enabled = function()
-      -- disable completion in comments
-      local context = require 'cmp.config.context'
-      -- keep command mode completion enabled when cursor is in a comment
-      if vim.api.nvim_get_mode().mode == 'c' then
-        return true
-      else
-        return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-      end
-    end,
-    snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body)
-      end
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    formatting = {
-      format = function(_, vim_item)
-        vim_item.kind = (M.cmp_kinds[vim_item.kind] or '') .. vim_item.kind
-        return vim_item
-      end,
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        -- elseif luasnip.expand_or_locally_jumpable() then
-        --   luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+	cmp.setup({
+		enabled = function()
+			-- disable completion in comments
+			local context = require("cmp.config.context")
+			-- keep command mode completion enabled when cursor is in a comment
+			if vim.api.nvim_get_mode().mode == "c" then
+				return true
+			else
+				return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+			end
+		end,
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+		window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		},
+		formatting = {
+			format = function(_, vim_item)
+				vim_item.kind = (M.cmp_kinds[vim_item.kind] or "") .. vim_item.kind
+				return vim_item
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				-- elseif luasnip.expand_or_locally_jumpable() then
+				--   luasnip.expand_or_jump()
+				elseif has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
 
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        -- elseif luasnip.jumpable(-1) then
-        --   luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<C-n>"] = cmp.mapping(function(fallback)
-        if luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
-        else
-          fallback()
-        end
-      end, {"i", "s"}),
-      ["<C-p>"] = cmp.mapping(function(fallback)
-        if luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {"i", "s"})
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-      { name = 'nvim_lsp_signature_help' },
-      { name = 'buffer' },
-    })
-  })
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				-- elseif luasnip.jumpable(-1) then
+				--   luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<C-n>"] = cmp.mapping(function(fallback)
+				if luasnip.expand_or_locally_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<C-p>"] = cmp.mapping(function(fallback)
+				if luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+		}),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp", sorting = { priority_weight = 5 } },
+			{ name = "luasnip", sorting = { priority_weight = 3 } },
+			{ name = "nvim_lsp_signature_help" },
+			{ name = "buffer" },
+		}),
+	})
 
-  cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    },
-    formatting = {
-      format = function(_, vim_item)
-        vim_item.kind = ""
-        return vim_item
-      end
-    }
-  })
+	cmp.setup.cmdline("/", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = {
+			{ name = "buffer" },
+		},
+		formatting = {
+			format = function(_, vim_item)
+				vim_item.kind = ""
+				return vim_item
+			end,
+		},
+	})
 
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' },
-      { name = 'cmdline' }
-    }),
-    formatting = {
-      format = function(_, vim_item)
-        vim_item.kind = ""
-        return vim_item
-      end
-    }
-  })
+	cmp.setup.cmdline(":", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "path" },
+			{ name = "cmdline" },
+		}),
+		formatting = {
+			format = function(_, vim_item)
+				vim_item.kind = ""
+				return vim_item
+			end,
+		},
+	})
 end
 
 return M
