4e51a18d84b0abc8c419afbec26a3142aa1f595f
Danmar Varela <vareladanmar@gmail.com>
2022-09-08 11:02:20 +0800

    Add publish diagnostic handler

--- dot_config/nvim/lua/grapevim/plug_configs/handlers.lua
+++ dot_config/nvim/lua/grapevim/plug_configs/handlers.lua
@@ -1,3 +1,12 @@
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	virtual_text = {
+		spacing = 5,
+		severity_limit = "Warning",
+	},
+	update_in_insert = true,
+})
