14f214d09620c54eeb76de5d93663e8307a4c5e8
Danmar Varela <dan@Danmars-MacBook-Air.local>
2022-10-30 13:14:33 +0800

    Modify icons

--- .config/nvim/after/plugin/lspzero.lua
+++ .config/nvim/after/plugin/lspzero.lua
@@ -1,36 +1,9 @@
 local ok, lspzero = pcall(require, "lsp-zero")
+local icons_ok, icons = pcall(require, "dan.icons")
 
-if not ok then
+if not ok or not icons_ok then
 	return
 end
-
-local cmp_kinds = {
-	Text = "  ",
-	Method = "  ",
-	Function = "  ",
-	Constructor = "  ",
-	Field = "  ",
-	Variable = "  ",
-	Class = "  ",
-	Interface = "  ",
-	Module = "  ",
-	Property = "  ",
-	Unit = "  ",
-	Value = "  ",
-	Enum = "  ",
-	Keyword = "  ",
-	Snippet = "  ",
-	Color = "  ",
-	File = "  ",
-	Reference = "  ",
-	Folder = "  ",
-	EnumMember = "  ",
-	Constant = "  ",
-	Struct = "  ",
-	Event = "  ",
-	Operator = "  ",
-	TypeParameter = "  ",
-}
 
 local lsp_formatting = function(bufnr)
 	vim.lsp.buf.format({
@@ -49,7 +22,7 @@ lspzero.setup_nvim_cmp({
 	formatting = {
 		fields = { "kind", "abbr" },
 		format = function(_, vim_item)
-			vim_item.kind = cmp_kinds[vim_item.kind] or ""
+			vim_item.kind = icons.kind[vim_item.kind] or ""
 			return vim_item
 		end,
 	},
