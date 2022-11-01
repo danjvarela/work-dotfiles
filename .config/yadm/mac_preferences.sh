f0b44a880f592551a73749bcf39e064d185453cb
Danmar Varela <dan@Danmars-MacBook-Air.local>
2022-11-02 04:57:51 +0800

    Add mac preferences

--- .config/yadm/mac_preferences.sh
+++ .config/yadm/mac_preferences.sh
@@ -5,4 +5,23 @@ defaults write InitialKeyRepeat -int 10
 defaults write KeyRepeat -int 1
 
 # Dock
+# size
+defaults write tilesize -int 40
+# autohide
+defaults write com.apple.dock autohide -bool false
+# autohide (animation duration)
+defaults write com.apple.dock autohide-time-modifier -float 0.4
+# autohide (animation delay)
+defaults write com.apple.dock autohide-delay -float 0
+
+
+# Trackpad
+# tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+# Finder
+# view as list
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+# show path bar
+defaults write com.apple.finder ShowPathbar -bool true
 
