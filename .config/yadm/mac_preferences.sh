83c21ad8637ce552bc9550040b26835b09ca7328
Danmar Varela <dan@Danmars-MacBook-Air.local>
2022-11-02 07:27:54 +0800

    Change mac preferences

--- .config/yadm/mac_preferences.sh
+++ .config/yadm/mac_preferences.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 # Adjust the keyboard repeat rate
-defaults write InitialKeyRepeat -int 10
-defaults write KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
 
 # Dock
 # size
 defaults write tilesize -int 40
 # autohide
-defaults write com.apple.dock autohide -bool false
+defaults write com.apple.dock autohide -bool true
 # autohide (animation duration)
 defaults write com.apple.dock autohide-time-modifier -float 0.4
 # autohide (animation delay)
 defaults write com.apple.dock autohide-delay -float 0
+killAll Dock
 
 
 # Trackpad
@@ -25,4 +26,5 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad -bool true
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # show path bar
 defaults write com.apple.finder ShowPathbar -bool true
+killAll Finder
 
