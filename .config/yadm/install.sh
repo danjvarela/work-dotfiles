6922930a79e677eb01a8eddda43fcee58cdb73ba
Danmar Varela <dan@Danmars-MacBook-Air.local>
2022-11-02 01:45:45 +0800

    Add script to install prerequisites

---
+++ .config/yadm/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# installs homebrew if it isn't installed yet
+if ! command -v brew >/dev/null 2>&1; then
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# install yadm
+brew install yadm
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# clone dotfiles repo and bootstrap it
+yadm clone https://github.com/danjvarela/dotfiles.git
