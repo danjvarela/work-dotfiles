409b4b59f16bf942e449d80e569cd0d1f6f749c6
Danmar Varela <dan@Danmars-MacBook-Air.local>
2022-11-02 09:49:01 +0800

    Filter fonts

--- .config/yadm/iterm.sh
+++ .config/yadm/iterm.sh
@@ -1,16 +1,60 @@
 #!/bin/sh
 
+fontlist=(
+"JetBrains Mono Bold Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono Bold Italic Nerd Font Complete.ttf"
+"JetBrains Mono Bold Nerd Font Complete Mono.ttf"
+"JetBrains Mono Bold Nerd Font Complete.ttf"
+"JetBrains Mono ExtraBold Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono ExtraBold Italic Nerd Font Complete.ttf"
+"JetBrains Mono ExtraBold Nerd Font Complete Mono.ttf"
+"JetBrains Mono ExtraBold Nerd Font Complete.ttf"
+"JetBrains Mono ExtraLight Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono ExtraLight Italic Nerd Font Complete.ttf"
+"JetBrains Mono ExtraLight Nerd Font Complete Mono.ttf"
+"JetBrains Mono ExtraLight Nerd Font Complete.ttf"
+"JetBrains Mono Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono Italic Nerd Font Complete.ttf"
+"JetBrains Mono Light Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono Light Italic Nerd Font Complete.ttf"
+"JetBrains Mono Light Nerd Font Complete Mono.ttf"
+"JetBrains Mono Light Nerd Font Complete.ttf"
+"JetBrains Mono Medium Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono Medium Italic Nerd Font Complete.ttf"
+"JetBrains Mono Medium Nerd Font Complete Mono.ttf"
+"JetBrains Mono Medium Nerd Font Complete.ttf"
+"JetBrains Mono Regular Nerd Font Complete Mono.ttf"
+"JetBrains Mono Regular Nerd Font Complete.ttf"
+"JetBrains Mono SemiBold Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono SemiBold Italic Nerd Font Complete.ttf"
+"JetBrains Mono SemiBold Nerd Font Complete Mono.ttf"
+"JetBrains Mono SemiBold Nerd Font Complete.ttf"
+"JetBrains Mono Thin Italic Nerd Font Complete Mono.ttf"
+"JetBrains Mono Thin Italic Nerd Font Complete.ttf"
+"JetBrains Mono Thin Nerd Font Complete Mono.ttf"
+"JetBrains Mono Thin Nerd Font Complete.ttf"
+)
+
+fontMissing=false
 # install JetbrainsMono nerd font
-if [ ! -f $HOME/Library/Fonts/JetBrains Mono Regular Nerd Font Complete.ttf ]; then
+for font in "${fontlist[@]}"; do
+  if [ ! -f "$HOME/Library/Fonts/$font" ]; then
+    fontMissing=true
+  fi
+done
+
+if $fontMissing; then
   echo "\nInstalling JetBrainsMono Nerd Font"
   cd $HOME/Library/Fonts/
   curl -fLo "JetBrainsMono.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
   yes A | unzip JetBrainsMono.zip
-  rm -v JetBrains*Windows*
-  rm -v JetBrainsMono.zip readme.md
+  rm JetBrains*Windows*
+  rm JetBrains*Mono*NL*
+  rm JetBrainsMono.zip readme.md
   cd $HOME
+  break
 else
-  echo "JetBrains Nerd font already installed."
+  echo "\nJetBrainsMono Nerd Font already installed."
 fi
 
 echo "\nSetting up iterm2"
