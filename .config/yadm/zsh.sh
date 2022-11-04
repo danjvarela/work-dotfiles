79c0583dc702f548b37e18ae7f28cb77050c72b6
Danmar Varela <dan@Danmars-MacBook-Air.local>
2022-11-04 23:26:24 +0800

    Fix zsh.sh not working the first time

--- .config/yadm/zsh.sh
+++ .config/yadm/zsh.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ZSH_CUSTOM="$HOME/.zsh"
+
 # fish-like syntax highlighting for zsh
 echo "\nInstalling zsh syntax highlighting"
 [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
