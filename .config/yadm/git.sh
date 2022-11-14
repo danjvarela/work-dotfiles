53bb96ff071947627b4d755210c7bcfff7f5300e
Dan <vareladanmarj@gmail.com>
2022-11-15 01:37:45 +0800

    Add git configuration

---
+++ .config/yadm/git.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# configure git
+echo "\nSetting up Git"
+git config --global user.name "Dan"
+git config --global user.email "vareladanmarj@gmail.com"
+git config --global core.editor "nvim"
+git config --global init.defaultBranch "master"
