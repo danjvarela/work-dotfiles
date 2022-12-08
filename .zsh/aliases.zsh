98c396b178c790e8f3a2b5e993746dbaed2f4d2c
Dan <vareladanmarj@gmail.com>
2022-12-09 01:30:50 +0800

    add alias to format files in a rails project

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,3 +2,9 @@ alias lg=lazygit
 alias reload="omz reload"
 alias dev="cd $PROJECTS"
 alias mux=tmuxinator
+
+ff(){
+  find . -iname '*.erb' -exec htmlbeautifier {} \;
+  standard --fix
+  standardrb --fix
+}
