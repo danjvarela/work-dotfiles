01f55580e92baa8acf1da3b467a0f85c5920c494
Danmar Varela <vareladanmarj@gmail.com>
2023-05-08 02:23:09 +0800

    update neovim config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,14 +1,7 @@
 -- Disable autoformat
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "*" },
+ pattern = { "*" },
   callback = function()
     vim.b.autoformat = false
   end,
-})
-
--- Open all folds by default
-vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost", "BufWinEnter" }, {
-  pattern = { "*" },
-  command = "normal zR",
-  group = vim.api.nvim_create_augroup("OpenAllFolds", {})
 })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@ vim.opt.cursorline = false
 vim.opt.guicursor = ""
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 999
