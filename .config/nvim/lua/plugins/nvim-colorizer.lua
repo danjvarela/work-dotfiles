cfb4cc02c9923ec44f3495b87a88490faf3b3de5
Danmar Varela <danmarvarela@VTIMEs-MacBook-Pro.local>
2023-07-26 13:40:48 +0800

    enable colorizer on json files

--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -2,7 +2,8 @@ return {
   "norcalli/nvim-colorizer.lua",
   config = function()
     require('colorizer').setup({
-      css = { css = true }
+      css = { css = true },
+      json = { css = true }
     })
   end
 }
