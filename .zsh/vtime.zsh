62e88c66eb94df9aefa4f341559b793107a8018c
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-09-27 14:00:34 +0800

    add useful scripts for work

---
+++ .zsh/vtime.zsh
@@ -0,0 +1,16 @@
+# collection of useful shortcuts for work
+
+ppp() {
+  declare -A themePorts
+  themePorts[sssbet]=3200
+  themePorts[hugebet]=3201
+  themePorts[spaceman]=3202
+  themePorts[amusino]=3203
+  themePorts[777bet]=3204
+  themePorts[kash777]=3205
+  THEME=$1 yarn dev -p $themePorts[$1]
+}
+
+ptest() {
+  THEME=$1 yarn theme && tsc --noEmit --project tsconfig.json
+}
