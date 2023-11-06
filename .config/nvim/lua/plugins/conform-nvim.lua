bdaaff2a12793462babd928434a900fdf2fb9b6c
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-11-06 15:04:53 +0800

    remove null-ls, use conform.nvim

---
+++ .config/nvim/lua/plugins/conform-nvim.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { { "prettierd", "prettier" } },
+      javascriptreact = { { "prettierd", "prettier" } },
+      css = { { "prettierd", "prettier" } },
+      json = { { "prettierd", "prettier" } },
+      yaml = { { "prettierd", "prettier" } },
+      markdown = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
+      typescriptreact = { { "prettierd", "prettier" } },
+    },
+  },
+}
