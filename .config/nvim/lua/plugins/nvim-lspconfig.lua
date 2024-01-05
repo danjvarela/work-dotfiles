8eab3cacfc59deef845bf9a92f7309e3d379c97f
Dan Varela <danmar.dev.ph@tripleonetech.net>
2024-01-05 20:43:58 +0800

    remove yabai and skhd; update nvim config

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = { "jose-elias-alvarez/typescript.nvim" },
   opts = {
     -- make sure mason installs the server
     servers = {
-      tsserver = {},
       solargraph = {},
       tailwindcss = {
         settings = {
@@ -19,17 +17,5 @@ return {
         },
       }
     },
-    setup = {
-      tsserver = function(_, opts)
-        require("lazyvim.util").lsp.on_attach(function(client, buffer)
-          if client.name == "tsserver" then
-            vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-            vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-          end
-        end)
-        require("typescript").setup({ server = opts })
-        return true
-      end,
-    },
-  },
+  }
 }
