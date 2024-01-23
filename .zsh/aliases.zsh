cf117dc25a0c5f4ba99676733e38149f4a678681
Dan Varela <danmar.dev.ph@tripleonetech.net>
2024-01-23 17:26:04 +0800

    remove tmux ressurect and continnuum; add alias to creating tmux session in a directory

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,9 @@
+start_work_session() {
+  local current_directory_name=$(basename "$PWD")
+  tmuxinator start work_session -n "$current_directory_name" .
+}
+
 alias lg=lazygit
 alias reload="omz reload"
 alias mux=tmuxinator
+alias sws="start_work_session"
