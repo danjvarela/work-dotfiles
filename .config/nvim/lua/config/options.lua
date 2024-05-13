c5b22cbb5cc439d7335e867a38d5d39605172571
Dan Varela <danmar.dev.ph@tripleonetech.net>
2024-05-13 13:50:00 +0800

    feat: modify config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.opt.cursorline = false
 vim.opt.guicursor = ""
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 999
+vim.opt.conceallevel = 0
+vim.opt.wrap = true
