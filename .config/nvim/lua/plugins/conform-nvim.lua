89018dedfcd58fd3b62260a28947f0890c00294e
Dan Varela <danmar.dev.ph@tripleonetech.net>
2024-06-11 00:11:02 +0800

    feat: add ruby formatter and make sure the formatters are installed

--- .config/nvim/lua/plugins/conform-nvim.lua
+++ .config/nvim/lua/plugins/conform-nvim.lua
@@ -2,6 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      ruby = { "standardrb" },
       lua = { "stylua" },
       ["javascript"] = { "prettierd" },
       ["javascriptreact"] = { "prettierd" },
