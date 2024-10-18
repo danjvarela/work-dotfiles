8ad44b2a5da1bf804968d192922f004b8fbadbec
Dan Varela <danmar.dev.ph@tripleonetech.net>
2024-10-18 13:48:13 +0800

    remove iterm opacity, add karabiner element keybindings

--- .config/nvim/lua/plugins/gruvbox-material.lua
+++ .config/nvim/lua/plugins/gruvbox-material.lua
@@ -4,8 +4,8 @@ return {
   priority = 1000,
   config = function()
     vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_background = "hard"
-    vim.g.gruvbox_material_transparent_background = 2
+    vim.g.gruvbox_material_background = "medium"
+    vim.g.gruvbox_material_transparent_background = 0
     vim.g.gruvbox_material_current_word = "high contrast background"
   end,
 }
